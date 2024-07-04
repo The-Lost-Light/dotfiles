7d1d14409c154467b1ba0dcc049c59141628921e
mono <boyhuanggordon@gmail.com>
2024-07-05 00:13:29 +0800

    2024/07/05

--- .config/ags/src/bar/modules/title.ts
+++ .config/ags/src/bar/modules/title.ts
@@ -1,27 +1,23 @@
-import Icon from "types/widgets/icon";
-import Label from "types/widgets/label";
-
 import Hyprland from "@services/hyprland";
-
-const Application = await Service.import("applications");
+import Applications from "@services/applications";
 
 export const abc = () =>
 	Widget.Label({
 		class_name: "client-title",
 		label: Hyprland.active
 			.bind("client")
-			.as(client => Application.query(client.class)[0]?.name ?? Hyprland.getClient(client.class)),
+			.as(client => Applications.query(client.class)[0]?.name ?? Hyprland.getClient(client.class)),
 	});
 
 export default () =>
 	Widget.Box({
 		class_name: "client-title",
-		spacing: 4,
-		children: [Widget.Icon(), Widget.Label()],
+		spacing: 8,
 	}).hook(Hyprland.active.client, self => {
-		const wmclass = Hyprland.active.client.class;
-		const entry = Application.query(wmclass)[0];
-		(self.children[0] as Icon<unknown>).icon = entry?.icon_name ?? "";
-		(self.children[1] as Label<unknown>).label = entry?.name ?? Hyprland.getClient(wmclass) ?? "";
-		if ((self.children[1] as Label<unknown>).label === "") self.visible = false;
+		const entry = Applications.getEntry(Hyprland.active.client.class);
+		const icon = entry?.icon_name ?? "";
+		const title = entry?.name ?? Hyprland.getClient(Hyprland.active.client.address)?.title ?? "";
+		self.children = [Widget.Icon(icon), Widget.Label(title)];
+		self.children[0].visible = !!icon;
+		self.visible = !!title;
 	});
