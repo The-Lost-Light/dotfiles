211deb7da5fb447193bdd6d2056aecf19d093966
mono <boyhuanggordon@gmail.com>
2024-08-26 05:49:29 +0800

    2024/08/26

--- .config/ags/src/bar/modules/workspaces.ts
+++ .config/ags/src/bar/modules/workspaces.ts
@@ -1,34 +1,48 @@
 import Hyprland from "@services/hyprland";
 import hover from "@lib/hover";
 
-const length = 10;
+const workspaces = () => [
+	...Hyprland.workspaces
+		.flatMap(workspace => (workspace.id >= 0 ? workspace.id : []))
+		.sort((i, j) => i - j)
+		.map(id =>
+			Widget.Button({
+				class_name: "workspace",
+				label: id.toString(),
+				on_clicked: self => {
+					Hyprland.changeWorkspace(id);
+					self.label = id.toString();
+				},
+				on_hover: self => {
+					if (Hyprland.monitors.length > 1 && id === Hyprland.active.workspace.id) self.label = "󰯍";
+				},
+				setup: self => hover.off(self, () => (self.label = id.toString())),
+			}).hook(Hyprland.active, self => {
+				const is_focused = id === Hyprland.getClient(Hyprland.active.client.address)?.workspace.id;
+				const is_monitored = Hyprland.workspaces.some(
+					workspace => workspace.id === id && workspace.monitorID === Hyprland.active.monitor.id,
+				);
+				self.toggleClassName("focused", is_focused);
+				self.toggleClassName("unmonitored", !is_monitored);
+			}),
+		),
+	Widget.Button({
+		child: Widget.Label("+"),
+		on_clicked: () => Hyprland.changeWorkspace("emptym"),
+		setup: self =>
+			Utils.idle(() => {
+				if (!self.is_destroyed) self.visible = !Hyprland.is_full(10);
+			}),
+	}),
+];
 
 export default () =>
 	Widget.EventBox({
 		class_name: "workspaces",
-		onScrollUp: () => Hyprland.changeWorkspace("+1"),
-		onScrollDown: () => Hyprland.changeWorkspace("-1"),
+		onScrollUp: () => Hyprland.changeWorkspace("m-1"),
+		onScrollDown: () => Hyprland.changeWorkspace("m+1"),
 
-		child: Widget.Box({
-			children: [
-				...Array.from({ length }, (_, id) => id + 1).map(id =>
-					Widget.Button({
-						class_name: Hyprland.active.workspace
-							.bind("id")
-							.as(active_id => `workspace ${active_id === id ? "focused" : "unfocused"}`),
-						visible: Hyprland.bind("workspaces").as(workspace => workspace.some(workspace => workspace.id === id)),
-						label: id.toString(),
-						on_clicked: self => (Hyprland.changeWorkspace(id), (self.label = id.toString())),
-						on_hover: self => Hyprland.monitors.length > 1 && id === Hyprland.active.workspace.id && (self.label = "󰯍"),
-						setup: self => hover.off(self, () => (self.label = id.toString())),
-					}),
-				),
-				Widget.Button({
-					child: Widget.Label("+"),
-					on_clicked: () => Hyprland.changeWorkspace("emptym"),
-				})
-					.hook(Hyprland, self => (self.visible = !Hyprland.is_full(length)), "workspace-added")
-					.hook(Hyprland, self => (self.visible = !Hyprland.is_full(length)), "workspace-removed"),
-			],
-		}),
+		child: Widget.Box()
+			.hook(Hyprland, self => (self.children = workspaces()), "workspace-added")
+			.hook(Hyprland, self => (self.children = workspaces()), "workspace-removed"),
 	});
